617d63b7b1351d5876414932678200d663e57106
idungstanley <sundaystanley56@gmail.com>
2022-11-09 21:12:11 +0100

    add changes on index.js

--- index.js
+++ index.js
@@ -1,40 +1,7 @@
 
+function ourFunction(p, o){
 
-function dcFunction(meeting) {
-
-return meeting + " maker of sons";
-
-}
-
-console.log(dcFunction('Instadad'));
-
-var myText = "I am a string";
-var newString = myText.replace("string", "sausage");
-console.log(newString);
-// the replace() string function takes a source string,
-// and a target string and replaces the source string,
-// with the target string, and returns the newly formed string
-
-var myVar = [ "I" , "love" , "milk" ,  "Produce"];
-var madeAString = myVar.join("  ");
-console.log(madeAString);
-// the join() function takes an array, joins
-// all the array items together into a single
-// string, and returns this new string
-
-function dunamisFunction(session) {
-
-return session + " was powerful with Tedd"
+console.log(p - o);
 
 }
-
-console.log(dunamisFunction("intelligence session"));
-
-let myName = "I'm a string";
-let anotherString = myName.replace("string", "boolean");
-console.log(anotherString);
-
-var myStar = ["dunamis", "was"  , "great"];
-var  changeString = myStar.join(' ');
-console.log(changeString);
-
+ourFunction(50 , 10);
