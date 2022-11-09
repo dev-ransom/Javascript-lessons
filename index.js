b330c01241a6fd61d5303d34c4ce5654bf39d85a
idungstanley <sundaystanley56@gmail.com>
2022-11-09 18:51:30 +0100

    add updates on index.js

--- index.js
+++ index.js
@@ -11,4 +11,14 @@ console.log(dcFunction('Instadad'));
 var myText = "I am a string";
 var newString = myText.replace("string", "sausage");
 console.log(newString);
+// the replace() string function takes a source string,
+// and a target string and replaces the source string,
+// with the target string, and returns the newly formed string
+
+var myVar = [ "I" , "love" , "milk" ,  "Produce"];
+var madeAString = myVar.join("  ");
+console.log(madeAString);
+// the join() function takes an array, joins
+// all the array items together into a single
+// string, and returns this new string
 
